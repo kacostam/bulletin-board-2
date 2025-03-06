efc1b299a29218a58a2de7ef15ffc00648e9e8e6
Karla Acosta <77418536+kacostam@users.noreply.github.com>
2025-03-06 01:12:31 +0000

    only show forms if user signed in

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -21,6 +21,7 @@ class PostsController < ApplicationController
     the_post = Post.new
     the_post.title = params.fetch("query_title")
     the_post.body = params.fetch("query_body")
+    the_post.user_id = current_user.id
     the_post.expires_on = params.fetch("query_expires_on")
     the_post.board_id = params.fetch("query_board_id")
 
